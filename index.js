71e3ba76cee446d5455d2c500bae1ccc904e3948
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-09-19 08:42:22 -0400

    still figuring this out

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let messages = []
+    for (let i = 0; i <names.length; i++ ) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+   const names = ('Guadalupe', 'Ollie' , 'Aki')
+   
+   // countdown??
+   function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+   }
+
